a3bf6d36b8280382fe2303f127b59cb8cfd4edb8
StevenMilbourne <52866836+StevenMilbourne@users.noreply.github.com>
2019-11-26 17:20:58 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,26 +17,37 @@ public:
 
     void onRequest(const Http::Request& request, Http::ResponseWriter response) override{
         UNUSED(request);
+		
+		// Figure out a way to snowboy into str, split str to reqs, then req separately as post, delete etc
         
-        if (request.resource() == "/smartlist") {
-            if (request.query().has("q")) {
-                std::string str = request.query().get("q").get();
-                std::cout << str << std::endl;
-                updateList(list, str);
-                list->display();
-                response.send(Pistache::Http::Code::Ok, str);
+        if (request.resource() == "/") {
+            if (request.method() == Http::Method::Post ) {
+				if (request.query().has("q")){
+					std::string str = request.query().get("q").get();
+					std::cout << str << std::endl;
+					updateList(list, str);
+					list->display();
+					response.send(Http::Code::Ok, str);
+				}
+				else{
+					response.send(Http::Code::Bad_Request, "Bad Request. Possibly Due To Empty String");
+				}
+
             }
-            
+            else if (request.method() == Http::Method::Get){
+				Http::serveFile(response, "../html/smartmirror.html", MIME(Text, Html));
+			}
+			
+			else if(request.method() == Http::Method::Delete) {
+				list->reset();
+				response.send(Pistache::Http::Code::Ok, "List Reset");
+			}
+			
             else{
-                response.send(Pistache::Http::Code::Ok, "String was empty");
+                response.send(Http::Code::Method_Not_Allowed, "Method Not Allowed");
             }
         }
-        
-        
-        else if (request.resource() == "/smartmirror") {
-            Http::serveFile(response, "../html/smartmirror.html", MIME(Text, Html));
-        }
-        
+
         
         else if (request.resource() == "/json") {
             response.headers().add<Pistache::Http::Header::ContentType>(MIME(Application, Json));
@@ -44,15 +55,8 @@ public:
         }
         
         
-        else if(request.resource() == "/reset") {
-            list->reset();
-            response.send(Pistache::Http::Code::Ok, "List Reset");
-        }
-        
         else{
-            
-            response.send(Pistache::Http::Code::Ok, "ok");
-            
+            response.send(Http::Code::Bad_Request, "Bad Request");
         }
     }
 };
